a9c07abae21aaa6c829958dc28fceede7ec2ffdf
João Vinaud <leyenasd@gmail.com>
2020-08-03 13:56:24 -0300

    Layout da Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import './assets/styles/global.css'
 
+import Landing from './pages/Landing'
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello WOrld</h1>
-    </div>
+    <Landing/>
   );
 }
 
