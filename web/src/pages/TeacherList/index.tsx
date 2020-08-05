2335459eb9a767e1a288d983d8a9cd4910cb231b
João Vinaud <leyenasd@gmail.com>
2020-08-05 13:45:06 -0300

    chore: Formulário de cadastro de professor

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,14 +3,40 @@ import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import './styles.css';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function TeacherList(){
     return(
      <div id="page-teacher-list" className="contaienr">
       <PageHeader title="Estes são os Proffys disponívies.">
         <form id="search-teachers">
-          <Input name="subject" label="Matéria"/> 
-          <Input name="week_day" label="Dia da semana"/> 
+        <Select name="subject" label="Matéria"
+                    options={[
+                        { value: 'Artes', label: 'Artes' },
+                        { value: 'Biologia', label: 'Biologia' },
+                        { value: 'Ciências', label: 'Ciências' },
+                        { value: 'Educação Física', label: 'Educação Física' },
+                        { value: 'Física', label: 'Física' },
+                        { value: 'Geografia', label: 'Geografia' },
+                        { value: 'História', label: 'História' },
+                        { value: 'Matemática', label: 'Matemática' },
+                        { value: 'Português', label: 'Português' },
+                        { value: 'Química', label: 'Química' },
+                      ]}
+                    /> 
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
           <Input type="time" name="time" label="Hora"/> 
         </form>
       </PageHeader>
