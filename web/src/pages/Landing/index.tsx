a8d45dbd45fa723a4b4891c5d040b891f77eab7d
João Vinaud <leyenasd@gmail.com>
2020-08-05 13:53:51 -0300

    chore: Listar total de conexões na landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React,  { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -12,6 +14,17 @@ import purpleHeartIcon from '../../assets/images//icons/purple-heart.svg';
 
 
 function Landing() {
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
     return(
         <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -39,7 +52,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
       </div>
