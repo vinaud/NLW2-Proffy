ad6a4624f33c5dea9b55e7b3122af6462be27e43
João Vinaud <leyenasd@gmail.com>
2020-08-07 13:57:32 -0300

    chore: COmponente TeacherItem

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {View, Image, Text} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler'
+import styles from './styles';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+
+function TeacherList(){
+    return(
+        <View style={styles.container}>
+            <View style={styles.profile}>
+            <Image style={styles.avatar} source={{ uri: 'https://github.com/vinaud.png'}}></Image>
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>João Vinaud</Text>
+                    <Text style={styles.subject}>Física</Text>
+                </View>
+
+            </View>
+            <Text style={styles.bio}>Lorem ipsum dolor </Text>
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora { '' }
+                    <Text style={styles.priceValue}>
+                    R$20,00
+                </Text>
+                </Text>
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={styles.favoriteButton}>
+                        <Image source={heartOutlineIcon}/>
+
+                    </RectButton>
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon}/>
+                        <Text>Entrar em contato</Text>
+
+                    </RectButton>
+                </View>
+
+            </View>
+                
+        </View>
+    )
+}
+
+export default TeacherList;
--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View,ScrollView } from 'react-native';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 import styles from './styles';
 
 function Favorites() {
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys disponíveis" />
+      <ScrollView style={styles.teacherList} contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}>
+      <TeacherItem/>
+      <TeacherItem/>
+      <TeacherItem/>
+      </ScrollView>
     </View>
   );
 }
